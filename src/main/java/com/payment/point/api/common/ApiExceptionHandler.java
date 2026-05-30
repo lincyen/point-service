50b13c1ea263012467e9e277996c19faa0b178c2
Lincyen <lincyen@gmail.com>
2026-05-30 18:22:56 +0900

    handleRequestParameterException 추가

--- src/main/java/com/payment/point/api/common/ApiExceptionHandler.java
+++ src/main/java/com/payment/point/api/common/ApiExceptionHandler.java
@@ -2,11 +2,14 @@ package com.payment.point.api.common;
 
 import com.payment.point.support.ApiException;
 import com.payment.point.support.ErrorCode;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -20,6 +23,17 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(ErrorCode.INVALID_PARAMETER));
+    }
+
+    @ExceptionHandler({
+            ConstraintViolationException.class,
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class
+    })
+    public ResponseEntity<ErrorResponse> handleRequestParameterException(Exception exception) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponse(ErrorCode.INVALID_PARAMETER));
